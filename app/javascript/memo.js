8aca389e6d0ffbf75261d2b1f86fdfc680a004af
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-06 14:10:18 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -3,9 +3,13 @@ function post (){
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
+
     const XHR = new XMLHttpRequest();
+    
     XHR.open("POST", "/posts", true);
+    
     XHR.responseType = "json";
+    
     XHR.send(formData);
   });
 };
